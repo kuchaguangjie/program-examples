f38c8052fb3a953f0bb0265a626bbea56e6602b3
eric <kuchaguangjie@gmail.com>
2025-01-31 17:32:00 +0800

    ...

--- basics/favorites/anchor/programs/favorites/src/lib.rs
+++ basics/favorites/anchor/programs/favorites/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 // Our program's address!
 // This matches the key in the target/deploy directory
-declare_id!("ww9C83noARSQVBnqmCUmaVdbJjmiwcV9j2LkXYMoUCV");
+declare_id!("HXYWZ945xVaxaGwi42K1PAeoBJczKyWoo4dWRucvFZpi");
 
 // Anchor programs always use 8 bits for the discriminator
 pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
